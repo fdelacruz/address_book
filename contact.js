6ffb388142f264a8eabfa13e766396ca9c4769ac
Francisco De La Cruz <helloquico@gmail.com>
2015-06-14 18:38:36 -0400

    Parse phone number

--- contact.js
+++ contact.js
@@ -4,4 +4,8 @@ Contact.parseName = function(str) {
 	return str.split(',')[0].trim();
 };
 
+Contact.parseNumber = function(str) {
+	return str.split(',')[1].trim();
+};
+
 module.exports = Contact;
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var Contact = require('./contact')
 
-var name = Contact.parseName("John Smith,604-123-9090")
+var number = Contact.parseNumber("John Smith,604-123-9090")
 
-console.log(name) // -> should print "John Smith"
+console.log(number) // -> should print "604-123-9090"
