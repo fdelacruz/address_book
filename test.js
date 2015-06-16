80cab8f1a0ea9542b374b0af18212094298aa110
Francisco De La Cruz <helloquico@gmail.com>
2015-06-16 10:08:14 -0400

    Parse command line arguments

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
-var Contact = require('./contact');
+var Command = require('./command');
 
-var contacts = [ { name: "John Smith", number: "604-123-9090" } ];
-
-var contact = 'John Smith';
-
-Contact.findContacts(contact, function(err) {
-	console.log('success');
-});
+console.log(Command.getOperation());
+console.log(Command.getOperationData());
