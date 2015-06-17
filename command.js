79fac2709f16a3751a68366573e9e0fc8dcf2f98
Francisco De La Cruz <helloquico@gmail.com>
2015-06-17 17:06:59 -0400

    Implement contact add command

--- command.js
+++ command.js
@@ -8,4 +8,12 @@ Command.getOperationData = function() {
 	return process.argv[3];
 };
 
+Command.add = function(done) {
+	var Contact = require('./contact');
+	var data = this.getOperationData();
+	var contactObj = Contact.createContact(data);
+
+	Contact.saveContact(contactObj, done);
+};
+
 module.exports = Command;
